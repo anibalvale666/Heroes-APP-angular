43f89fe24d15bb1fc265fa29fde1f9ff617fee6c
anibalvale666 <anibal.valenzuela@ucsp.edu.pe>
2022-04-13 14:11:30 -0500

    routes protection

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router,
+              private authService: AuthService) { }
+  
+  login() {
+    this.authService.login()
+      .subscribe( ({id}) => {
+        // console.log(resp);
+        if(id) {
+          this.router.navigate(['/heroes']);
+        }
+      })
   }
 
 }
